ea0ec9c938abca3eb6716769d14ee6fe24d86cd8
Alexander Molchanov <bonext@gmail.com>
2011-09-01 18:32:03 +0400

    Memory screws up in looped OpenCV calls. Work in progress

--- flash/camlib.cpp
+++ flash/camlib.cpp
@@ -1,10 +1,56 @@
 #include "AS3.h"
 #include "opencv/cv.h"
+#include <stdio.h>
+
+extern long int lrintf(float f)
+{
+  /* Implements the default IEEE 754-1985 rounding mode */
+  long * fp = (long *)&f;
+  int sign = (*fp) >> 31;
+  int exponent = 23 + 0x7f - (((*fp) >> 23) & 0xff);
+  unsigned int fraction = ((*fp) & 0x7fffff) | 0x800000;
+  long result = fraction >> exponent;
+  -- exponent;
+  if (fraction & (1 << exponent)) { // fraction >= 0.5
+    if (!(fraction & ~(-1 << exponent))) // fraction == 0.5
+      if (!(result & 1)) // result is even
+        return sign ? -result : result;
+    ++ result;
+  }
+  return sign ? -result : result;
+}
+
+extern long int lrint(double d)
+{
+  return lrintf((float)d);
+}
+
+extern long int lrintl(long double ld)
+{
+  return lrint((double)ld);
+}
+
+extern long long int llrint(double x)
+{
+  return (long long int) lrint(x);
+}
+
+extern long long int llrintf(float x)
+{
+  return (long long int) lrintf(x);
+}
+
+extern long long int llrintl(long double x)
+{
+  return (long long int) lrintl(x);
+}
 
 //screen buffer size
 int bufferSize;
 char* buffer;
 int frameWidth, frameHeight, frameChannels;
+
+//cv:: Mat frame,gs_frame;
 
 //FLASH: C memory init function
 static AS3_Val initByteArray(void* self, AS3_Val args)
@@ -33,9 +79,17 @@ static AS3_Val setFrameParams(void* self, AS3_Val args)
   return 0;
 }
 
+void cvMagic()
+{
+  cv::Mat frame(frameHeight,frameWidth,CV_8UC3,(void*)buffer);
+  cv::Mat gs_frame(frameHeight,frameWidth,CV_8UC1);
+}
+
 //FLASH: OpenCV image fiddling
 static AS3_Val testCV(void* self, AS3_Val args)
 {
+  cvMagic();
+
   return 0;
 }
 
